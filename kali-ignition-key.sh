4b23240d566373bf70515a02e326227709bd1b46
Nicolas <nvformella@gmail.com>
2022-11-15 09:54:54 -0600

    Added some things

--- kali-ignition-key.sh
+++ kali-ignition-key.sh
@@ -36,7 +36,8 @@ then
 	exit
 fi
 
-who am i | awk '{print $1}'
+BLUE "Updating repositories..."
+sudo apt install -y git
 
 BLUE "[*] Pimping my kali..."
 git clone https://github.com/Dewalt-arch/pimpmykali.git /home/$user/pimpmykali
@@ -149,10 +150,22 @@ BLUE "[*] Installing Nim..."
 sudo -u kali curl https://nim-lang.org/choosenim/init.sh -sSf | sh
 echo 'export PATH=/home/$user/.nimble/bin:$PATH' >> /home/$user/.zshrc
 
+BLUE "Installing pdfcrack..."
+sudo apt install -y pdfcrack
+
+BLUE "Installing GIMP..."
+sudo apt install -y gimp
+
+BLUE "[*] Installing Sublime Text..."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get install sublime-text -y
+
+BLUE "Installing Spotify..."
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client -y
 
 # Comment out any of the following dotfiles to keep current files
 function dotfiles(){
